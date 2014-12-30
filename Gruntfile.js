bde6f3f79a047c86d6e4feca6140d6594cd7411d
Duncan Beaton <dunckr@gmail.com>
2014-12-30 22:02:08 +0000

    init

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt);
+    grunt.loadNpmTasks('assemble');
+
+    grunt.initConfig({
+        assemble: {
+            options: {
+                flatten: true,
+                layoutdir: 'templates/layouts',
+                partials: 'templates/partials/*.hbs',
+                helpers: 'templates/helpers/*.js',
+            },
+            content: {
+                options: {
+                    data: 'templates/data/*.json'
+                },
+                src: ['contents/**/*.html'],
+                dest: 'public/'
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['contents/*.html', 'contents/scss/*.scss', 'templates/**/*.hbs'],
+                tasks: ['sass', 'assemble', 'copy'],
+            },
+        },
+        connect: {
+            options: {
+                port: 9000,
+                base: './public/',
+                livereload: true
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    base: './public/'
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {},
+                files: {
+                    'public/css/main.css': 'contents/scss/main.scss'
+                }
+            }
+        },
+        copy: {
+            dev: {
+                files: [{
+                    expand: true,
+                    cwd: 'contents/',
+                    src: ['**/*.js', '**/*.css', '**/*.png', '**/*.eot', '**/*.svg', '**/*.ttf', '**/*.woff', '**/*.otf'],
+                    dest: 'public/'
+                }, ]
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'contents/',
+                    src: ['*.ico'],
+                    dest: 'public/'
+                }, ]
+            }
+        },
+        clean: ['public']
+    });
+
+    grunt.registerTask('default', ['sass', 'assemble', 'copy', 'connect', 'watch']);
+    grunt.registerTask('build', ['clean', 'assemble', 'copy', 'sass']);
+};
