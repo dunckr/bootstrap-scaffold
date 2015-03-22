a89ebc259da6c85702fec9f36192f5ade317bce7
Duncan Beaton <dunckr@gmail.com>
2015-03-22 11:33:40 +0000

    Updates

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
 'use strict';
 
-$(document).ready(function() {
-    $('.hero').css('height', ($(window).height() - $('header').outerHeight()) + 'px');
-    $('#scroll-hero').click(function() {
-        $('html,body').animate({
-            scrollTop: $("#hero-bloc").height()
-        }, 'slow');
-    });
-});
-
-$(window).resize(function() {
-    $('.hero').css('height', ($(window).height() - $('header').outerHeight()) + 'px');
-});
+$(function() {});
