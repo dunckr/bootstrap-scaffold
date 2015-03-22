08e02093b608c759d8240529acbe1a85ceda53db
Duncan Beaton <dunckr@gmail.com>
2015-03-22 11:43:21 +0000

    add readme

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,79 +2,79 @@
 
 module.exports = function(grunt) {
 
-    require('load-grunt-tasks')(grunt);
-    grunt.loadNpmTasks('assemble');
+  require('load-grunt-tasks')(grunt);
+  grunt.loadNpmTasks('assemble');
 
-    grunt.initConfig({
-        assemble: {
-            options: {
-                flatten: true,
-                layoutdir: 'templates/layouts',
-                partials: 'templates/partials/*.hbs',
-                helpers: 'templates/helpers/*.js',
-            },
-            content: {
-                options: {
-                    data: 'templates/data/*.json'
-                },
-                src: ['contents/**/*.html'],
-                dest: 'public/'
-            }
+  grunt.initConfig({
+    assemble: {
+      options: {
+        flatten: true,
+        layoutdir: 'templates/layouts',
+        partials: 'templates/partials/*.hbs',
+        helpers: 'templates/helpers/*.js',
+      },
+      content: {
+        options: {
+          data: 'templates/data/*.json'
         },
-        watch: {
-            scripts: {
-                files: ['contents/**', 'templates/**'],
-                tasks: ['sass', 'assemble', 'copy'],
-            },
-        },
-        connect: {
-            options: {
-                port: 9000,
-                base: './public/',
-                livereload: true
-            },
-            livereload: {
-                options: {
-                    open: true,
-                    base: './public/'
-                }
-            }
-        },
-        sass: {
-            dist: {
-                options: {},
-                files: {
-                    'public/css/main.css': 'contents/scss/main.scss'
-                }
-            }
-        },
-        copy: {
-            dev: {
-                files: [{
-                    expand: true,
-                    cwd: 'contents/',
-                    src: ['**/*.js', '**/*.css', '**/*.png'],
-                    dest: 'public/'
-                }, ]
-            },
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: 'contents/',
-                    src: ['*.ico'],
-                    dest: 'public/'
-                }, ]
-            }
-        },
-        'gh-pages': {
-            options: {
-                base: 'public'
-            },
-            src: ['**']
-        },
-        clean: ['public']
-    });
+        src: ['contents/**/*.html'],
+        dest: 'public/'
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['contents/**', 'templates/**'],
+        tasks: ['sass', 'assemble', 'copy'],
+      },
+    },
+    connect: {
+      options: {
+        port: 9000,
+        base: './public/',
+        livereload: true
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: './public/'
+        }
+      }
+    },
+    sass: {
+      dist: {
+        options: {},
+        files: {
+          'public/css/main.css': 'contents/scss/main.scss'
+        }
+      }
+    },
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          cwd: 'contents/',
+          src: ['**/*.js', '**/*.css', '**/*.png'],
+          dest: 'public/'
+        }, ]
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'contents/',
+          src: ['*.ico'],
+          dest: 'public/'
+        }, ]
+      }
+    },
+    'gh-pages': {
+      options: {
+        base: 'public'
+      },
+      src: ['**']
+    },
+    clean: ['public']
+  });
 
-    grunt.registerTask('default', ['sass', 'assemble', 'copy', 'connect', 'watch']);
-    grunt.registerTask('build', ['clean', 'assemble', 'copy', 'sass']);
+  grunt.registerTask('default', ['sass', 'assemble', 'copy', 'connect', 'watch']);
+  grunt.registerTask('deploy', ['clean', 'assemble', 'copy', 'sass', 'gh-pages']);
 };
---
+++ README.md
@@ -0,0 +1,21 @@
+# [Scaffold Bootstrap](http://dunckr.github.io/bootstrap-scaffold/)
+
+Scaffold provides generic [Bootstrap](http://getbootstrap.com) components ready to paste into your page. Zero CSS or JS dependancies other than Bootstrap.
+
+Use either the [Handlebars](http://handlebarsjs.com/) [templates](https://github.com/dunckr/bootstrap-scaffold/tree/master/templates/partials) or the source on the [demo page](http://dunckr.github.io/bootstrap-scaffold/).
+
+#### Install
+
+`npm i`
+
+#### Run
+
+`grunt `
+
+#### Deploy
+
+`grunt deploy`
+
+## License
+
+MIT © [Duncan Beaton](http://dunckr.com)
